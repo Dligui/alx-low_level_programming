49636bf013dd93a72f5d27faaf08eaa369f7aca5
DLIGUI <dligui.ghizlane@gmail.com>
2023-05-26 07:39:59 -0700

    variadic function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,26 +1,20 @@
 #include "variadic_functions.h"
-
+#include <stdarg.h>
 /**
- * sum_them_all - calculate the sum of all the parameters
- * @n : number of arguments passed into the function
- *
- * return: the resulting sum
- */
-
+* sum_them_all - Returns the sum of all its paramters.
+* @n: The number of paramters passed to the function.
+* @...: A variable number of paramters to calculate the sum of.
+*
+* Return: If n == 0 - 0.
+* Otherwise - the sum of all parameters.
+*/
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	int sum=0;
-
-	va_list list;
-
-	va_start(list, n);
-
-	for (i=0; i<n;i++)
-	{
-		sum+=va_arg(list, int);
-	}
-
-	va_end(list);
-	return (sum);
+va_list ap;
+unsigned int i, sum = 0;
+va_start(ap, n);
+for (i = 0; i < n; i++)
+sum += va_arg(ap, int);
+va_end(ap);
+return (sum);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,32 +1,31 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
- *print_strings : function that print strings 
- *@separator : that seperate between strings
- *@n : number of arguments
- */
-
+* print_strings - Prints strings, followed by a new line.
+* @separator: The string to be printed between strings.
+* @n: The number of strings passed to the function.
+* @...: A variable number of strings to be printed.
+*
+* Description: If separator is NULL, it is not printed.
+* If one of the strings if NULL, (nil) is printed instead.
+*/
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list list;
-	char *str;
-
-	va_start(list,n);
-	for(i=0;i<n;i++)
-		 {
-			 str = va_arg(list, char *);
-			 if (!str)
-				 str = "nill";
-			 if (!separator)
-				 printf("%s",str);
-			 else if(separator && i==0 )
-				printf("%s", str);
-				
-			 else 
-				 printf("%s%s", separator, str); 	
-		}
-
-		printf("\n");
-		va_end(list);
+va_list strings;
+char *str;
+unsigned int index;
+va_start(strings, n);
+for (index = 0; index < n; index++)
+{
+str = va_arg(strings, char *);
+if (str == NULL)
+printf("(nil)");
+else
+printf("%s", str);
+if (index != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+printf("\n");
+va_end(strings);
 }
